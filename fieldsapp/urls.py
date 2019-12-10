8b8548857528590f231e035599b2c6ac224d0e0f
Bektur <tip.beka99@gmail.com>
2019-12-10 11:22:25 +0600

    main project

---
+++ fieldsapp/urls.py
@@ -0,0 +1,30 @@
+from django.urls import path
+from .views import (
+    AboutPostView,
+    TeamPostView,
+    NewsPostView,
+    BlogPostView,
+    ContactPostView,
+    PoleListView,
+    PoleUpdateView,
+    PoleDeleteView,
+    PoleCreateView,
+    PoleCreateImageView,
+    PoleDetailView,
+)
+urlpatterns = [
+    path('about/', AboutPostView.as_view(), name='about'),
+    path('team/', TeamPostView.as_view(), name='team'),
+    path('news/', NewsPostView.as_view(), name="news"),
+    path('blog/', BlogPostView.as_view(), name='blog'),
+    path('contact/', ContactPostView.as_view(), name='contact'),
+    path('<int:pk>/edit/', PoleUpdateView.as_view(), name='pole_edit'),
+    path('<int:pk>/', PoleDetailView.as_view(), name='pole_detail'),
+    path('<int:pk>/delete/', PoleDeleteView.as_view(), name='pole_delete'),
+    path('new/', PoleCreateView.as_view(), name='pole_new'),
+    path('', PoleListView.as_view(), name='pole_list'),
+    path('<int:pk>/create_image/', PoleCreateImageView.as_view(), name='pole_create_image'),
+]
+
+
+
